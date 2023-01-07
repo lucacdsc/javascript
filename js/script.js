7647428b5f1f78c792459113eac5201446f81b53
Luca Clemente <clementluca6@gmail.com>
2023-01-07 19:52:07 -0300

    add files to the main branch

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Olá, eu sou um teste.")
+
+const nome = 'luca'
+let age = 21
+
+/*
+declarar uma variável sem valor retornará undefined
+*/
+
+let favoriteColor = 'brown'
+
+let favoriteFood;
+
+favoriteFood = 'gyoza'
+
